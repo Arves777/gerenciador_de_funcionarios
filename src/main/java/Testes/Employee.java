9cd351bb8955b513183fee43b3ded4db3d5fe459
Adrian Rosselis <54713631+Arves777@users.noreply.github.com>
2025-02-08 10:20:57 -0300

    Update Employee.java

--- src/main/java/Testes/Employee.java
+++ src/main/java/Testes/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
     private String name;
     private double salary;
 
-    public Employer(int id, String name, double salary) {
+    public Employee(int id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
